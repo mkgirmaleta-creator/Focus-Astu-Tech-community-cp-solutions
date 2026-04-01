38d4e64a6f8b18236d1f81ef89f6e242468f3498
Tolesa Girma Leta <mkgirmaleta@gmail.com>
2026-04-01 13:40:05 -0700

    Create Boats-to- save.py
    solved problem on leetcode

---
+++ Boats-to- save.py
@@ -0,0 +1,25 @@
+ people.sort()
+        n=len(people)
+        l,r=0,n-1
+        c=0
+        while l<r:
+            if people[l]+people[r]<=limit:
+                c+=1
+                l+=1
+                r-=1
+            elif people[l]>=limit:
+                c+=1
+                l+=1
+            elif people[r]>=limit:
+                c+=1
+                r-=1
+            elif people[l]+people[r]>=limit:
+                 c+=1
+                 r-=1
+        if r==l:
+            c+=1
+
+        return c
+
+
+
